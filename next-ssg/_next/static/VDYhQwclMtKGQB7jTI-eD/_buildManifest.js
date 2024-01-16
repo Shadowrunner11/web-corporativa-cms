eeea895fefa0a344fc7e91f4fd7e5a7ca7170df5
Shadowrunner11 <Shadowrunner11@users.noreply.github.com>
2024-01-16 05:44:15 +0000

    deploy: 067d318a33667ba050013e4c9b18a6aeeaff16c5

---
+++ next-ssg/_next/static/VDYhQwclMtKGQB7jTI-eD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-51f98368f6fc08fd.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ next-ssg/_next/static/chunks/app/page-54ac00cb2423a7cb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{9874:function(){}},function(n){n.O(0,[810,569,744],function(){return n(n.s=9874)}),_N_E=n.O()}]);
